e14536a47766ad5c74d79ffd120cf0650497479b
aLeuleu <aurelien.levra@gmail.com>
2022-12-21 00:13:38 +0100

    quicksort_WIP

--- short_sort.c
+++ short_sort.c
@@ -6,7 +6,7 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 13:45:33 by alevra            #+#    #+#             */
-/*   Updated: 2022/12/20 16:13:00 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2022/12/21 00:08:40 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,26 @@ void	short_sort(t_stacks_pair *stacks)
 	}
 	while (b->size > 0)
 		execute_command("pa", stacks, &stacks, depth++);
+	execute_command("END", stacks, &stacks, depth++);
+}
+
+void	quicksort_stacks(t_stacks_pair *stacks)
+{
+	size_t		pivot_pos;
+	int			pivot;
+	size_t		depth;
+	int			i;
+	int			
+
+	i = 0;
+	depth = 0;
+	pivot_pos = stacks->a->size / 2;
+	pivot = stacks->a->tab[pivot_pos];
+	while (i < stacks->a->size)
+	{
+		if (stacks->a->tab[i] > pivot)
+			
+	}
+	execute_command("END", stacks, &stacks, depth++);
 	execute_command("END", stacks, &stacks, depth++);
 }
