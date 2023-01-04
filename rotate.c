1dfacc4889f80b9c905a079b98681592524e4e55
aLeuleu <aurelien.levra@gmail.com>
2023-01-04 18:14:05 +0100

    rename var

--- rotate.c
+++ rotate.c
@@ -6,27 +6,27 @@
 /*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 13:43:04 by alevra            #+#    #+#             */
-/*   Updated: 2022/12/17 15:18:59 by alevra           ###   ########lyon.fr   */
+/*   Updated: 2023/01/04 18:12:16 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	rotate(t_stack *A)
+void	rotate(t_stack *a)
 {
 	int	last;
 	int	tmp;
 	int	i;
 
 	i = 0;
-	last = A->size - 1;
-	tmp = A->tab[0];
+	last = a->size - 1;
+	tmp = a->tab[0];
 	while (i < last)
 	{
-		A->tab[i] = A->tab[i + 1];
+		a->tab[i] = a->tab[i + 1];
 		i++;
 	}
-	A->tab[last] = tmp;
+	a->tab[last] = tmp;
 }
 
 void	rotate_reverse(t_stack *a)
